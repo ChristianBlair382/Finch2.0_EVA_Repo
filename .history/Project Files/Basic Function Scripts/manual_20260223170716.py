4957c18bd10e5ba1f017a2ebb037edd5bad0a174
Christian Blair <c.blair@marquette.edu>
2026-02-23 17:36:55 -0600

    Manual Controller Implemented
    Version 1
    
    - manual.py (run from VS Code) allows the user to control the Finch's movement using the keyboard.
    - Testing: successful
    - Comments: very jittery and could be smoother.

---
+++ .history/Project Files/Basic Function Scripts/manual_20260223170716.py
@@ -0,0 +1,10 @@
+from BirdBrain import Finch
+import keyboard
+
+myFinch = Finch()
+
+while True:
+  
+    print(keyboard.read_key())
+    if keyboard.read_key() == "a":
+        break
---
+++ .history/Project Files/Basic Function Scripts/manual_20260223172800.py
@@ -0,0 +1,18 @@
+from BirdBrain import Finch
+import keyboard
+
+myFinch = Finch()
+
+while True:
+    print(keyboard.read_key())
+    if keyboard.read_key() == "a":
+        myFinch.setTurn('L', 1, 100)
+    elif keyboard.read_key() == "d":
+        myFinch.setTurn('R', 1, 100)
+    elif keyboard.read_key() == "w":
+        myFinch.setMove('F', 1, 100)
+    elif keyboard.read_key() == "s":
+        myFinch.setMove('B', 1, 100)w
+    elif keyboard.read_key() == "q":
+        myFinch.stop()
+        break
